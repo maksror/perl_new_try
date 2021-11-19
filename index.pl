c67fc8e9c5939ce5966ef287b754d5ffc48ebffd
root <root@194-67-86-115.cloudvps.regruhosting.ru>
2021-11-19 15:45:31 +0300

    Добавлено удаление контакта Добавлены обработки ошибок Изменены описания функций и добавлены новые коментарии к коду Изменён INSERT запрос на добалвение контакта(теперь запрос через placeholder-ы выполняется)

--- index.pl
+++ index.pl
@@ -1,17 +1,25 @@
 #!/usr/bin/perl
 
-use lib "./";
+use Modern::Perl;
 use Mojolicious::Lite -signatures;
+
+use lib "./";
 use mysql_operations qw (show_all search add_contact remove_contact modify_contact);
 
-use strict;
-use warnings;
+#use strict;
+#use warnings;
 
 get '/' => sub ($c) {
     my @rows = show_all ();
-    $c->stash( 'len'=>$#rows );
-    $c->render( template => 'index', 
-                rows => \@rows, );
+    if ( $rows[0] eq "Something") {
+        $c->stash( error => @rows );
+        $c->render( template => 'alert' );
+    }
+    else {
+        $c->stash( 'len'=>$#rows );
+        $c->render( template => 'index', 
+                    rows => \@rows, );
+    }
 };
 
 post '/' => sub ($c) {
@@ -31,9 +39,32 @@ post '/search' => sub ($c) {
     my $pattern = $c->param( 'data' );
     my @rows = search( $pattern );
     
-    $c->stash( 'len'=>$#rows );
-    $c->render( template => 'index', 
-                rows => \@rows, );
+    # Если поиск завершился успешно
+    if ( grep /^ARRAY/, $rows[0]) {
+        $c->stash( 'len'=>$#rows );
+        $c->render( template => 'index',
+                    rows => \@rows, );
+    }
+    # Поиск завершился ошибкой
+    else {
+        $c->stash( error => @rows);
+        $c->render( template => 'alert' );
+    }
+};
+
+post '/delete' => sub ($c) {
+    my $candidat = $c->param( 'data' );
+
+    my $candidat_validation = validate_remove_condidat($candidat);
+
+    if ( $candidat_validation eq 0 ){
+        remove_contact($candidat); 
+        $c->redirect_to( '/' );
+    } 
+    else {
+        $c->stash( error => $candidat_validation);
+        $c->render( template => 'alert' );
+    }
 };
 
 app->start;
--- mysql_operations.pm
+++ mysql_operations.pm
@@ -10,20 +10,33 @@ my $PASS = "si7ughaehuoy7quaHahp";
 
 # Подключение к БД
 # Входные данные: нет
-# Выходные данные: линка на соединение к БД
+# Выходные данные: 
+#   Успешное подключение: линка с mysql подключением
+#   Внутреняя ошибка: строка с ошибкой
 sub create_connect {
-    return DBI -> connect( "DBI:mysql:database=$DB;", $USER, $PASS, {'RaiseError' => 1} );
+    my $link = DBI -> connect( "DBI:mysql:database=$DB;", $USER, $PASS, {'RaiseError' => 0} ) 
+    # Если по какой либо причине подключение не удалось, то возвращаем указанную ошибку.
+    or return( "Something went wrong! Please contact us with this error" );
+    # Если всё хорошо, то возвращаем линку
+    return( $link );
 }
 
 # Проверка имени и номера
 # Входные данные: имя, номер
-# Выходные данные: 0 если проверка пройдена или текст ошибки
+# Выходные данные:
+#   Успешная валидация: 0
+#   Проваленная валидация: строка с ошибкой
 sub validate_data {
-    my ($name, $number) = shift;
+    my ($name, $number) = @_;
+
+    # Если ввели пустые данные, то ошибка
     if ( length $name == 0 or length $number == 0) {
         return( "Empty values is not allowed" );
     }
+
+    # Если номер создержит что-то кроме символа "+" и цифр - тригерим ошибку
     if ( $number =~ m/^[\d\+]*$/ ) {
+        # Если такой номер уже существует
         for my $contact ( show_all() ) {
             if ( $number eq ${$contact}[1] ) {
                 return( "This number is already used" );
@@ -38,11 +51,14 @@ sub validate_data {
 
 # Тривальный поиск по полному сопадению значений
 # Входные данные: строка поиска, ссылка на массив с массивами.
-# Выходные данные: массив масивов с парами [имя, телефон]
+# Выходные данные: 
+#   Удачный поиск: массив масивов с парами [имя, телефон]
+#   Поиск завершился ошибкой: строка с ошибкой
 sub basic_search {
     my ($pattern, $all) = @_;
 
     my @result;
+
     for my $contact ( @{ $all } ) {
         if ( grep /^$pattern$/, @{$contact} ) {
             push( @result, $contact );
@@ -54,11 +70,14 @@ sub basic_search {
 
 # Расширенный(умный) поиск
 # Входные данные: строка поиска, ссылка на массив с массивами.
-# Выходные данные: массив масивов с парами [имя, телефон]
+# Выходные данные: 
+#   Удачный поиск: массив масивов с парами [имя, телефон]
+#   Поиск завершился ошибкой: строка с ошибкой
 sub advanced_search {
     my ($search_string, $all) = @_;
-    my @result;
+
     my $len = length $search_string;
+    my @result;
 
     # Добавляем один любой символ в каждое место строки поиска
     for my $i (0 .. $len) {
@@ -87,51 +106,85 @@ sub advanced_search {
 
 
     # Исключаем до m БУКВ из строки поиска.
-    # Проверка реализована для случайно добавленной БУКВЫ в номере(хотя проверка будет смотреть паттерн и в имени).
+    # Проверка была убрана из-за отсутвия необходимости. Блок кода выше реализует почти тоже самое
+    # Плодить ещё одну проверку тут безсмысленно.
 
     return (@result);
 }
 
 # Обёртка для поиска. Тригерит сначала тривиальный поиск и если там пусто, то задействует расширенный.
 # Входные данные: строка поиска
-# Выходные данные: массив масивов с парами [имя, телефон]
+# Выходные данные: 
+#   Удачный поиск: массив масивов с парами [имя, телефон]
+#   Поиск завершился ошибкой: строка с ошибкой
 sub search {
     my $search_string = shift;
+
+    # Если строка пустая, то тригерим ошибку
+    if ( length $search_string < 1 ) {
+        return( "Search string is empty" );
+    }
+
     my @all = show_all();
+    if ( grep /^Something/, $link ) {
+        return( $link );
+    }
+
     # Экранируем все спец символы в строке поиска.
     $search_string = "\Q$search_string\E";
 
+    # Производим поиск по полному совпадению со строкой
     my @result = basic_search( $search_string, \@all );
-    # Если в паттерне поиска допустили ошибку:
-    if( !@result ) {
+
+    # Если полное совпадение не дало результатов, то производим расширенный поиск:
+    if ( !@result ) {
         push( @result, advanced_search( $search_string, \@all ) );
     }
-
-    return( @result );
+    
+    if ( !@result ) {
+        return( "The search did not find any suitable contacts" );
+    }
+    else {
+        return( @result );
+    }
 }
 
 # Выборка всех данных из БД
 # Входные данные: нет
-# Выходные данные: массив массивов с парами [имя, телефон]
+# Выходные данные: 
+#   Успешное получение данных: массив массивов с парами [имя, телефон]
+#   Внутреняя ошибка(подключения к БД или SELECT запроса): строка с ошибкой
 sub show_all {
     my $link = create_connect();
+    if ( grep /^Something/, $link ) {
+        return ($link);
+    }
     my $query = "SELECT * FROM `contacts`;";
-    my $query_result = $link -> selectall_arrayref( $query );
+    my $query_result = $link -> selectall_arrayref( $query ) 
+    # Если запрос неудчаный, тригерим шаблон ошибки
+    or return ( "Something went wrong! Please contact us with this error" ) ;
     $link -> disconnect;
     return( @$query_result );
 }
 
 # Добавление контакнта в БД
 # Входные данные: имя, номер
-# Выходные данные: 0 если контакт добавлен или текст ошибки
+# Выходные данные: 
+#   Удачное добавление: 0
+#   Неудачное добавление: строка с ошибкой
 sub add_contact {
-    # Проверка 
     my ($name, $number) = @_;
+    # Валидация данных на добавление
     my $is_valid = validate_data( $name, $number );
+
     if ( $is_valid eq 0 ) { 
         my $link = create_connect();
-        my $query = "INSERT INTO `contacts` (Name,Phone) VALUES (\"$name\",\"$number\");";
-        $link -> do( $query ) or die $link->errstr ;
+        # Если соединение не удалось - вызываешь ошибку
+        if ( grep /^Something/, $link ) {
+            return( $link );
+        }
+        my $query = "INSERT INTO `contacts` (Name,Phone) VALUES (?,?)";
+        $link -> do( $query, undef, ($name, $number) ) or return( $link->errstr );
         $link -> disconnect;
         return( 0 );
     } 
@@ -140,8 +193,42 @@ sub add_contact {
     }
 }
 
+# Валидация номера телефона для удаления
+# Входные данные: номер телефона
+# Выходные данные:
+#   Успешная проверка: 0
+#   Номер не прошёл проверку: строка с ошибкой
+sub validate_remove_condidat {
+    my $candidat = shift;
+    
+    if ( $candidat =~ m/^[\d\+]*$/ and length $candidat > 0 ) {
+        return( 0 );
+    }
+    else {
+        return( "Invalid Phone number" );
+    }
+}
+
+# Удаление контакта
+# Входные данные: номер телефона
+# Выходные данные:
+#   Успешное удаление: 0
+#   Ошибка при удалении: строка с ошибкой
 sub remove_contact {
-    #pass
+    my $phone = shift;
+
+
+    my $link = create_connect();
+    if ( grep /^Something/, $link ) {
+        return( $link );
+    }
+
+    my $query = "DELETE FROM `contacts` WHERE `Phone` = ?";
+    my $res = $link -> do( $query, undef, $phone ) or return( $link->errstr );
+    print ($res);
+    $link -> disconnect;
+    return( 0 );
+
 }
 
 sub modify {

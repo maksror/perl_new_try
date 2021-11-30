c58fff913de4632d7b74a1f30541799eb35e5cfb
maksror <maxror001@gmail.com>
2021-11-30 15:00:20 +0200

    Приведение кода к стандартам

--- index.pl
+++ index.pl
@@ -10,13 +10,15 @@ use mysql_operations;
 get '/' => sub ($c) {
     my $contacts = show_all ();
 
-    if (exists $contacts->{'Alert'}) {
+    if (exists $contacts->{ 'Alert' }) {
         $c->render( template => 'alert',
-                    alert => $contacts->{'Alert'}, );
+                    alert => $contacts->{'Alert'}, 
+        );
     }
     else {
         $c->render( template => 'index', 
-                    rows => $contacts, );
+                    rows => $contacts, 
+        );
     }
 };
 
@@ -24,11 +26,12 @@ get '/' => sub ($c) {
 post '/add' => sub ($c) {
     my $result = add_contact( $c->param ('name'), 
                               $c->param ('phone'),
-                            );
+    );
 
     # Возвращается всегда аллетр, поэтому задействуем только этот шаблон
     $c->render( template => 'alert',
-                alert => $result->{'Alert'}, );
+                alert => $result->{'Alert'}, 
+    );
 };
 
 # Поиск контакта
@@ -37,13 +40,15 @@ post '/search' => sub ($c) {
 
     my $contacts = search( $pattern );
     
-    if (exists $contacts->{'Alert'}) {
+    if (exists $contacts->{ 'Alert' }) {
         $c->render( template => 'alert',
-                    alert => $contacts->{'Alert'}, );
+                    alert => $contacts->{ 'Alert' }, 
+        );
     }
     else {
         $c->render( template => 'index',
-                    rows => $contacts, );
+                    rows => $contacts, 
+        );
 
     }
 };
@@ -53,19 +58,21 @@ post '/delete' => sub ($c) {
     my $candidat = $c->param( 'data' );
 
     # Производим уникальный поиск по паттерну
-    my $search_result = search_uniq_contact($candidat);
+    my $search_result = search_uniq_contact( $candidat );
     
     # Если поиск нашёл более одного значения или завершился ошибкой - выводим алерт
-    if (exists $search_result->{'Alert'}) {
+    if (exists $search_result->{ 'Alert' }) {
         $c->render( template => 'alert',
-                    alert => $search_result->{'Alert'}, );
+                    alert => $search_result->{ 'Alert' }, 
+        );
     }
     # Если поиск вернул лишь одно значение - удаляем контакт
     else {
-        my ($phone, undef) = each %$search_result;
-        my $remove_result = remove_contact($phone);
+        my ( $phone, undef ) = each %$search_result;
+        my $remove_result = remove_contact( $phone );
         $c->render( template => 'alert',
-                    alert => $remove_result->{'Alert'}, );
+                    alert => $remove_result->{ 'Alert' }, 
+        );
 
     }
 };
@@ -77,30 +84,37 @@ get '/modify' => sub($c) {
     # Производим уникальный поиск по паттерну
     my $search_result = search_uniq_contact( $candidat );
     # Если поиск нашёл более одного значения или завершился ошибкой - выводим алерт
-    if (exists $search_result->{'Alert'}) {
+    if (exists $search_result->{ 'Alert' }) {
         $c->render( template => 'alert',
-                    alert => $search_result->{'Alert'}, );
+                    alert => $search_result->{ 'Alert' }, 
+        );
     }
     # Поиск вернул одно значение - отрисовываем шаблон для редактирования
     else {
         $c->render( template => 'modify',
-                    rows => $search_result, );
+                    rows => $search_result, 
+        );
     }
 };
 
 # Модификация контакта
 post '/modify' => sub($c) {
-    my $old_name = $c->param( 'old_name' );
-    my $new_name = $c->param( 'new_name' );
+    my $old_name  = $c->param( 'old_name'  );
+    my $new_name  = $c->param( 'new_name'  );
     my $old_phone = $c->param( 'old_phone' );
     my $new_phone = $c->param( 'new_phone' );
 
     # Тригерим само изменение
-    my $result = modify( $old_name, $new_name, $old_phone, $new_phone );
+    my $result = modify_contact( $old_name, 
+                                 $new_name, 
+                                 $old_phone, 
+                                 $new_phone 
+    );
 
     # Возвращается всегда алерт(как при удачном редактировании так и при ошибке).
     $c->render( template => 'alert',
-                alert => $result->{'Alert'}, );
+                alert => $result->{ 'Alert' }, 
+    );
 };
 
 app->start;
--- mysql_operations.pm
+++ mysql_operations.pm
@@ -7,8 +7,8 @@ use Modern::Perl;
 my $dir_name = dirname(__FILE__);
 # Загружаем конфиг с атрибутами подключения к БД
 my %config = Config::General->new(
--ConfigFile => "$dir_name/config.cfg",
--InterPolateVars => 1,
+                                 -ConfigFile => "$dir_name/config.cfg",
+                                 -InterPolateVars => 1,
 )->getall;
 
 #****ФУНКЦИИ****
@@ -18,43 +18,49 @@ my %config = Config::General->new(
 # Выходные данные: 
 #   Успешное подключение: линка с mysql подключением
 sub create_connect {
-    my $link = DBI -> connect( "DBI:mysql:database=$config{DB};", 
-                                $config{USER}, $config{PASS}, 
-                                {"RaiseError" => 0, "mysql_enable_utf8" => 1,}) 
+    my $link = DBI->connect( "DBI:mysql:database=$config{ DB };", 
+                             $config{ USER }, $config{ PASS }, 
+                             { 
+                               'RaiseError' => 0, 
+                               'mysql_enable_utf8' => 1, 
+                             }
+    )
     # Если по какой либо причине подключение не удалось, то умираем. 
     # Конечному пользователю внутренние ошибки знать не нужно.
     or die $DBI::errstr;
     
     # Если всё хорошо, то возвращаем линку
-    return( $link );
+    return $link;
 }
 
 # Проверка имени и номера
 # Входные данные: имя, номер
 # Выходные данные:
 #   Успешная валидация: 0
-#   Проваленная валидация: хэш "Alert"=>"Оповещение"
+#   Проваленная валидация: хэш 'Alert'=>"Оповещение"
 sub validate_data {
-    my ($candidat_name, $candidat_phone) = @_;
+    my ( $candidat_name, $candidat_phone ) = @_;
 
     # Если ввели пустые данные, то ошибка
     if (length $candidat_name == 0 or length $candidat_phone == 0) {
-        return( {"Alert" => "Empty values is not allowed"} );
+        return { 'Alert' => 'Empty values is not allowed' };
     }
 
     # Если номер создержит что-то кроме символа "+" и цифр - тригерим ошибку
     if ($candidat_phone =~ m/^[\d\+]*$/) {
         my $all = show_all();
+
         # Если такой номер уже существует
         while (my ( $existing_phone, undef ) = ( each %{ $all } )) {
             if ($candidat_phone eq $existing_phone) {
-                return( {"Alert" => "This number is already used"} );
+                return { 'Alert' => 'This number is already used' };
             }
         }
-        return( 0 );
+
+        return 0;
     } 
     else {
-        return( {"Alert" => "Invalid phone number"} );
+        return { 'Alert' => 'Invalid phone number' };
     }
 }
 
@@ -63,28 +69,28 @@ sub validate_data {
 # Проверка строки на возможность её использования в регулярке /^$pattern$/ лежит на вызывающей стороне
 # Выходные данные: 
 #   Удачный поиск: ссылка на хэш с парами "Телефон" => "Имя"
-#   Поиск завершился ошибкой: хэш "Alert"=>"Оповещение"
+#   Поиск завершился ошибкой: хэш 'Alert'=>"Оповещение"
 sub basic_search {
-    my ($pattern, $all) = @_;
+    my ( $pattern, $all ) = @_;
 
     my %result;
 
     while (my ( $phone,$name ) = ( each %{ $all } )) {
         if (grep /$pattern/, ( lc( $phone ), lc( $name ) )) {
-            $result{$phone} = $name;
+            $result{ $phone } = $name;
         }
     }
 
-    return( \%result );
+    return \%result;
 }
 
 # Расширенный(умный) поиск
 # Входные данные: строка поиска, ссылка на хэш с результатами show_all.
 # Выходные данные: 
 #   Удачный поиск: ссылка на хэш с парами "Телефон" => "Имя"
-#   Поиск завершился ошибкой: хэш "Alert"=>"Оповещение"
+#   Поиск завершился ошибкой: хэш 'Alert'=>"Оповещение"
 sub advanced_search {
-    my ($search_string, $all) = @_;
+    my ( $search_string, $all ) = @_;
 
     my $len = length $search_string;
     my %result;
@@ -96,41 +102,41 @@ sub advanced_search {
         my $basic_result = basic_search( $pattern, $all );
         while (my( $phone, $name ) = ( each %{ $basic_result } )) {
             # Добавляем только ранее не найденные контакты
-            if (not exists $result{$phone}) {
-                $result{$phone} = $name;
+            if (not exists $result{ $phone }) {
+                $result{ $phone } = $name;
             }
         }
     }
 
     # Меняем до 2 символов в строке поиска на любой
     for my $i ( 0..( $len - 1 ) ) {
-        my $pattern_i = substr( $search_string, 0, $i ) . "\E.\Q" . substr( $search_string, $i+1 );
+        my $pattern_with_one_change = substr( $search_string, 0, $i ) . "\E.\Q" . substr( $search_string, $i+1 );
         for my $j ( 0..( $len - 1 ) ) {
-            my $pattern_j = substr( $pattern_i, 0, $j ) . "\E.\Q" . substr( $pattern_i, $j+1 ); 
+            my $pattern_with_two_changes = substr( $pattern_with_one_change, 0, $j ) . "\E.\Q" . substr( $pattern_with_one_change, $j+1 ); 
             # Ищем по получившемуся паттерну
-            my $basic_result = basic_search( $pattern_j, $all );
-            while (my( $phone, $name ) = ( each %{ $basic_result } )) {
+            my $basic_result = basic_search( $pattern_with_two_changes, $all );
+            while (my ( $phone, $name ) = ( each %{ $basic_result } )) {
                 # Добавляем только ранее не найденные контакты
-                if (not exists $result{$phone}) {
-                    $result{$phone} = $name;
+                if (not exists $result{ $phone }) {
+                    $result{ $phone } = $name;
                 }
             }
         }
     }
-    return( \%result );
+    return \%result;
 }
 
 # Обёртка для поиска. Тригерит сначала тривиальный поиск и если там пусто, то задействует расширенный.
 # Входные данные: строка поиска
 # Выходные данные: 
 #   Удачный поиск: ссылка на хэш с парами "Телефон" => "Имя"
-#   Поиск завершился ошибкой: ссылка на хэш "Alert"=>"Оповещение"
+#   Поиск завершился ошибкой: ссылка на хэш 'Alert'=>"Оповещение"
 sub search {
-    my $search_string = shift;
+    my ( $search_string ) = @_;
 
     # Если строка пустая, то тригерим ошибку
     if (length $search_string < 1) {
-        return( {"Alert" => "Search string is empty"} );
+        return { 'Alert' => 'Search string is empty' };
     }
 
     my $all = show_all();
@@ -148,10 +154,10 @@ sub search {
 
     # Если и расширенный поиск не дал результата, то добавляем оповещение
     if (!%$result) {
-        return( {"Alert" => "The search did not find any suitable contacts"} );
+        return { 'Alert' => 'The search did not find any suitable contacts' };
     }
 
-    return( $result );
+    return $result;
 }
 
 # Выборка всех данных из БД
@@ -161,25 +167,26 @@ sub search {
 sub show_all {
     my $link = create_connect();
 
-    my $query = "SELECT * FROM `contacts`";
-    my $query_result = $link -> selectall_hashref( $query, "Phone" ) or die $link->errstr;
-    $link -> disconnect;
+    my $query = 'SELECT * FROM `contacts`';
+    my $query_result = $link->selectall_hashref( $query, 'Phone' ) 
+    or die $link->errstr;
+    $link->disconnect;
 
     # Превращаем полученные данные в удобные для обработки - хэш телефон=>Имя(телефон Primary Key в БД).
     my %result;
     for my $phone ( keys %$query_result ) {
-        $result{$phone} = $query_result->{$phone}->{"Name"};
+        $result{ $phone } = $query_result->{ $phone }->{ 'Name' };
     }
 
-    return( \%result );
+    return \%result;
 }
 
 # Добавление контакнта в БД
 # Входные данные: имя, номер
 # Выходные данные: 
-#  Сыылка на хэш "Alert"=>"Результат"
+#  Сcылка на хэш 'Alert'=>"Результат"
 sub add_contact {
-    my ($name, $number) = @_;
+    my ( $name, $number ) = @_;
 
     # Валидация данных на добавление
     my $is_valid = validate_data( $name, $number );
@@ -187,13 +194,19 @@ sub add_contact {
     if ($is_valid eq 0) { 
         my $link = create_connect();
 
-        my $query = "INSERT INTO `contacts` (Name,Phone) VALUES (?,?)";
-        $link -> do( $query, undef, ($name, $number) ) or die $link->errstr;
-        $link -> disconnect;
-        return( {"Alert" => "Contact was successfully added"} );
+        my $query = 'INSERT INTO `contacts` (Name,Phone) VALUES (?,?)';
+        $link->do( 
+                  $query, 
+                  undef, 
+                  ( $name, $number ) 
+        ) 
+        or die $link->errstr;
+        $link->disconnect;
+
+        return { 'Alert' => 'Contact was successfully added' };
     } 
     else {
-        return( $is_valid );
+        return $is_valid;
     }
 }
 
@@ -204,24 +217,24 @@ sub add_contact {
 #   Успешный поиск(единственного совпадения с паттерном): ссылка на хэш с контактом
 #   Поиск завершился ошибкой или выдал более 1 результата: ссылка на хэш с оповещением
 sub search_uniq_contact {
-    my $candidat = shift;
+    my ( $candidat ) = @_;
 
-    my $search_result = search($candidat);
+    my $search_result = search( $candidat );
    
     # Если кандидат это номер телефона и присутвует в выборке
-    if (exists $search_result->{$candidat}) {
-        return ( {"$candidat" => $search_result->{$candidat}} );
+    if (exists $search_result->{ $candidat }) {
+        return { $candidat => $search_result->{ $candidat } };
     }
     # Если выборка вернула более одного результата
     elsif (keys %$search_result > 1) {
-        return( {"Alert" => "A search of your pattern returned more than one value."
-                           ." Please provide an identifier that is unique to the contact."})
+        return { 'Alert' => 'A search of your pattern returned more than one value.'
+                           .' Please provide an identifier that is unique to the contact.'};
     } 
 
     # В остальных случаях просто возвращаем результат(Alert или единственный найденный контакт).
     # Контакт возвращается когда пользователь искал по имени и оно оказалось уникальным
     # Остальные кейсы обрабатываются if-ом выше
-    return( $search_result );
+    return $search_result;
 }
 
 # Удаление контакта
@@ -229,45 +242,64 @@ sub search_uniq_contact {
 # Перед удалением вызвать search_uniq_contact и использовать телефон из результата.
 # Входные данные: номер телефона
 # Выходные данные:
-#   Ссылка на хэш "Alert" => "Результат"
+#   Ссылка на хэш 'Alert' => "Результат"
 sub remove_contact {
-    my $removable_phone = shift;
+    my ( $removable_phone ) = @_;
 
     my $link = create_connect();
 
-    my $query = "DELETE FROM `contacts` WHERE `Phone` = ?";
-    my $res = $link -> do( $query, undef, $removable_phone ) or die $link->errstr;
-    $link -> disconnect;
+    my $query = 'DELETE FROM `contacts` WHERE `Phone` = ?';
+    my $res = $link->do( 
+                        $query, 
+                        undef, 
+                        $removable_phone 
+    ) 
+    or die $link->errstr;
+    $link->disconnect;
+
     # Проверка на случай, если вызов функции будет выполнен с неверным параметром
-    if ($res eq "0E0"){
-        return( {"Alert" => "Phone number was not found"} ); 
+    if ($res eq "0E0") {
+        return { 'Alert' => 'Phone number was not found' }; 
     }
 
-    return( {"Alert" => "Contact was successfully removed"} );
+    return { 'Alert' => 'Contact was successfully removed' };
 }
 
 # Изменение контакта
 # Входные данные: старое/новое имя, старый/новый телефон
 # Выходные данные:
-# Ссылка на хэш "Alert" => "Результат"
-sub modify {
-    my ($old_name, $new_name, $old_phone, $new_phone) = @_;
+# Ссылка на хэш 'Alert' => "Результат"
+sub modify_contact {
+    my ( $old_name, $new_name, $old_phone, $new_phone ) = @_;
 
-    my $validate_result = validate_data($new_name,$new_phone);
+    # Валидация новых данных
+    my $validate_result = validate_data( $new_name, $new_phone );
+
+    # Если валидация успешная или старый телефон равен новому(и валидация тригерит ошибку по этому), то всё меняем данные
     if (
         $validate_result eq 0
-        || ( $validate_result->{"Alert"} eq "This number is already used"
-             && $old_phone eq $new_phone )
+        || ( 
+            $validate_result->{ 'Alert' } eq 'This number is already used'
+             && $old_phone eq $new_phone 
+           )
     ) {
-        my $query = "UPDATE `contacts` 
-                     SET `Phone` = ?, `Name` = ?
-                     WHERE `Phone` = ?";
+        my $query = q/
+                     UPDATE `contacts` 
+                        SET `Phone` = ?, `Name` = ?
+                      WHERE `Phone` = ?
+                     /;
         my $link = create_connect();
-        $link -> do( $query, undef, ($new_phone, $new_name, $old_phone) ) or die $link->errstr;
-        $link -> disconnect;
-        return( {"Alert" => "The contact has been successfully modified"} );
+        $link->do( 
+                 $query, 
+                 undef, 
+                 ( $new_phone, $new_name, $old_phone ) 
+        ) 
+        or die $link->errstr;
+        $link->disconnect;
+
+        return { 'Alert' => 'The contact has been successfully modified' };
     } 
     else {
-        return( $validate_result );    
+        return $validate_result;    
     }
 }
